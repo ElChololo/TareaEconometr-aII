a4e61bf81b8e8b9acdaab80bb9959df45c515fba
alvaro <aacevedod@fen.uchile.cl>
2022-05-27 19:17:55 -0400

    Se resuelve incongruencia con la base de datos(filtering) y se agrega respuesta item b)

--- Script_TareaII_Econometria.m
+++ Script_TareaII_Econometria.m
@@ -1,3 +1,4 @@
 obj = TareaEconometriaII('cps09mar/cps09mar.xlsx');
 df = obj.df;
-[MCO_Coef , Std_err_Coef] = obj.PreguntaA;
+obj = obj.PreguntaA;
+obj = obj.PreguntaB;
--- TareaEconometriaII.m
+++ TareaEconometriaII.m
@@ -1,33 +1,44 @@
 classdef TareaEconometriaII
     properties
-       df 
+       df
+       MCO_Coef
+       Std_err_Coef
+       theta
     end
     methods
         function obj = TareaEconometriaII(route) 
             %Inicialización de la tabla de datos
             df = readtable(route);
             % Observaciones relevantes
-            df=df(df{:,"race"}==7 & df{:,"female"} == 0,["earnings","education","age","race","female"]);
+            df=df(df{:,"hisp"}== 1 & df{:,"race"}== 1 & df{:,"female"} == 0,["hisp" "earnings","education","age","race","female"]);
             %log salario
             df{:,"earnings"}=log(df{:,"earnings"});
+            %Quitar menores de edad
             % construcción variable experiencia
-            exp= df{df{:,"age"}>=18,"age"}-(df{:,"education"}+6);
+            exp= df{:,"age"}-df{:,"education"}+6;
             exp_2 = (exp.^2)/100;
             obj.df = [df table(exp) table(exp_2)];
         end
-        function [MCO_Coef , Std_err_Coef] = PreguntaA(obj)
+        function [obj] = PreguntaA(obj)
            MCO = @(X,Y) (X'*X)\ (X'*Y);
            % Estimación de Coeficientes MCO
            X = [obj.df{:,["education","exp","exp_2"]}];
            X = [ones(length(X),1) X];
            Y = obj.df{:,"earnings"};
-           MCO_Coef = MCO(X,Y);
+           obj.MCO_Coef = MCO(X,Y);
            
            % Error Estándar robusto de los estimadores
-            residuos = Y - X* MCO_Coef;
-            Matriz_White = ((X.* residuos )'*(X.* residuos));
-            Std_err_Coef = (X'*X)\ Matriz_White / (X'*X);
-            Std_err_Coef = diag(Std_err_Coef);
+           residuos = Y - X* obj.MCO_Coef;
+           Matriz_White = ((X.* residuos )'*(X.* residuos));
+           Std_err_Coef = (X'*X)\ Matriz_White / (X'*X);
+           obj.Std_err_Coef = diag(Std_err_Coef);
+           
+        end
+        
+        function [obj] = PreguntaB(obj)
+           marginal_educ = obj.MCO_Coef(2);
+           marginal_exp = obj.MCO_Coef(3)+(2/100)*obj.MCO_Coef(4)*10;
+           obj.theta = marginal_educ/marginal_exp;
         end
     end
 end
