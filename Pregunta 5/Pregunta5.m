1be61c892f1b1f988106b7c90d65f5a375b8902a
ElChololo <aacevedod@fen.uchile.cl>
2022-06-02 19:09:21 -0400

    Se agrega wild boostrap con errores y apropiados junto a los errores estandar. Falta agregar los intervalos de confianza

--- Pregunta 5/Pregunta5.m
+++ Pregunta 5/Pregunta5.m
@@ -27,15 +27,18 @@ beta_bootstrap = zeros(6,B);
 for ii=1:B
    % hay que transformar los errores, pregunta para mí: aplicar por cluster
    % o a toda la población ?
-   residstar = datasample(e,obs);
-   ystar = zeros(obs);
+   ajuste_e_aux= binornd(1,ajust_p2,length(e),1);
+   ajuste_e =  (1 - sqrt(5))/2 * ajuste_e_aux + (1 + sqrt(5))/2 * (1 - ajuste_e_aux);
+   err_wild_bootrstarp = e .* ajuste_e;
+   residstar = datasample(err_wild_bootrstarp,obs);
+   ystar = zeros(obs,1);
    for jj=1:obs
-       ystar(jj)= mco_coef(1)*x(1,jj) + mco_coef(2)*x(2,jj) + mco_coef(3)*x(3,jj) ...
-              +mco_coef(4)*x(4,jj) +mco_coef(5)*x(5,jj)+mco_coef(6)*x(6,jj) + residstar(obs);
+       ystar(jj)= mco_coef(1)*x(jj,1) + mco_coef(2)*x(jj,2) + mco_coef(3)*x(jj,3) ...
+              +mco_coef(4)*x(jj,4) +mco_coef(5)*x(jj,5)+mco_coef(6)*x(jj,6) + residstar(jj);
    end
-   cte = ones(obs,1);
-   
-   X = [cte x];
-   betastar = (X'*X)\(X'*y);
-   beta_bootstrap(:,b);
+
+   betastar = (x'*x)\(x'*ystar);
+   beta_bootstrap(:,ii)= betastar;
 end
+
+error_estandar_b = std(beta_bootstrap,0,2);
