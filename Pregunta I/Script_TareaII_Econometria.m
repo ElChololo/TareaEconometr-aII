ec43168344c1e6bb81d4c4b0f751f84d10639a8e
ElChololo <aacevedod@fen.uchile.cl>
2022-06-03 01:05:41 -0400

    Se añade código Pregunta I

--- Pregunta I/Script_TareaII_Econometria.m
+++ Pregunta I/Script_TareaII_Econometria.m
@@ -1,4 +1,21 @@
 obj = TareaEconometriaII('cps09mar/cps09mar.xlsx');
 df = obj.df;
-obj = obj.PreguntaA;
-obj = obj.PreguntaB;
+%%
+obj =obj.PreguntaA;
+[obj.MCO_Coef , obj.Std_err_Coef]
+%%
+obj =obj.PreguntaB;
+obj.theta
+%%
+obj =obj.PreguntaC;
+obj.s_theta
+%%
+obj =obj.PreguntaD;
+obj.intervalo_conf_theta
+%%
+obj =obj.PreguntaE;
+obj.est_int_conf
+%%
+obj =obj.PreguntaF;
+obj.Int_Conf_nivel_predict
+obj.Int_Conf_nivel_exp_predict
--- Pregunta I/TareaEconometriaII.m
+++ Pregunta I/TareaEconometriaII.m
@@ -2,8 +2,15 @@ classdef TareaEconometriaII
     properties
        df
        MCO_Coef
+       residuos
        Std_err_Coef
+       var_betas
        theta
+       s_theta 
+       intervalo_conf_theta
+       est_int_conf
+       Int_Conf_nivel_predict
+       Int_Conf_nivel_exp_predict
     end
     methods
         function obj = TareaEconometriaII(route) 
@@ -29,8 +36,10 @@ classdef TareaEconometriaII
            
            % Error Estándar robusto de los estimadores
            residuos = Y - X* obj.MCO_Coef;
+           obj.residuos = residuos;
            Matriz_White = ((X.* residuos )'*(X.* residuos));
            Std_err_Coef = (X'*X)\ Matriz_White / (X'*X);
+           obj.var_betas = Std_err_Coef;
            obj.Std_err_Coef = diag(Std_err_Coef);
            
         end
@@ -39,6 +48,36 @@ classdef TareaEconometriaII
            marginal_educ = obj.MCO_Coef(2);
            marginal_exp = obj.MCO_Coef(3)+(2/100)*obj.MCO_Coef(4)*10;
            obj.theta = marginal_educ/marginal_exp;
+        end
+        
+        function [obj] = PreguntaC(obj)
+            mco_coef = obj.MCO_Coef;
+            R=[ 1/(mco_coef(3)+ (2/100)*obj.MCO_Coef(4)*10) ; (-1*mco_coef(2))/...
+                ((mco_coef(3)+ (2/100)*obj.MCO_Coef(4)*10)^2); (-1*mco_coef(2))/(...
+                (mco_coef(3)+ (2/100)*obj.MCO_Coef(4)*10)^2) * (2/100*10)];
+            obj.s_theta = sqrt(R'*obj.var_betas(2:end,2:end)*R);
+        end
+        function [obj] = PreguntaD(obj)
+            obj.intervalo_conf_theta = [obj.theta-1.64*obj.Std_err_Coef , obj.theta+1.64*obj.Std_err_Coef];
+        end
+        
+        function [obj] = PreguntaE(obj)
+           X = [1,12,20,400/100];
+           %Estimación MCO
+           lg_y_est = X*obj.MCO_Coef;
+           %Intervalo de Confianza
+           var_est = sqrt(X*obj.var_betas*X');
+           obj.est_int_conf = [lg_y_est - 1.96 * var_est,lg_y_est + 1.96 * var_est ];
+        end
+        function [obj] = PreguntaF(obj)
+            lg_y_predict = [1,16,5,25/100]*obj.MCO_Coef;
+            estimador_var_e = (obj.residuos' * obj.residuos)/(height(obj.df)-length(obj.MCO_Coef));
+            Error_Estandar_predict = sqrt(estimador_var_e +[1,16,5,25/100]*obj.var_betas*[1,16,5,25/100]');
+            exp(lg_y_predict)
+            obj.Int_Conf_nivel_predict = [lg_y_predict - 1.28 * Error_Estandar_predict, ...
+                lg_y_predict + 1.28 * Error_Estandar_predict];
+            obj.Int_Conf_nivel_exp_predict = [exp(lg_y_predict)- 1.28 * exp(Error_Estandar_predict), ...
+                exp(lg_y_predict)+ 1.28 * exp(Error_Estandar_predict)];
         end
     end
 end
